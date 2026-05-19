388ec8a013b3945b19aefc348e59fd21eb9dc081
bornfly <bornflyborntochange@outlook.com>
2026-05-20 04:12:39 +0800

    chore(gitignore): exclude .visual-cache from repo

--- .visual-cache/source-vite/deps/@codemirror_lang-css.js
+++
@@ -1,2 +0,0 @@
-import { i as defineCSSCompletionSource, n as cssCompletionSource, r as cssLanguage, t as css } from "./dist-By0ZBCx_.js";
-export { css, cssCompletionSource, cssLanguage, defineCSSCompletionSource };
--- .visual-cache/source-vite/deps/@codemirror_lang-html.js
+++
@@ -1,2 +0,0 @@
-import { a as htmlLanguage, i as htmlCompletionSourceWith, n as html, r as htmlCompletionSource, t as autoCloseTags } from "./dist-JVHdGyGy.js";
-export { autoCloseTags, html, htmlCompletionSource, htmlCompletionSourceWith, htmlLanguage };
